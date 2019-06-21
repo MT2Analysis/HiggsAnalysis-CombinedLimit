991470bfa24c5226c1bfc20ce5d6a9a94452a62a
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-06-21 14:48:44 +0200

    updates to ranking procedure

--- MT2Scripts/readAsymptoticLimits_forRanking.sh
+++ MT2Scripts/readAsymptoticLimits_forRanking.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 
 Model=$1
+#Model='T2qq_1200_850'
+
+workDir=/scratch/`whoami`/ranking/
+cd $workDir
 
 for i in $(ls ranking_${Model}/log_${Model}*Paper*post*)
 do
@@ -14,17 +18,17 @@ do
 	obslimit=$(grep "Observed" $i | awk '{print $5}')
 	if [[ $limit ]]
 	then
-            yield=$(grep rate /scratch/`whoami`/datacards_${Model}/datacard_${region}_m*_${Model}.txt | awk '{ sum+=$2} END {print sum}')
-            bkg=$(grep rate /scratch/`whoami`/datacards_${Model}/datacard_${region}_m*_${Model}.txt | awk '{ sum+=$3+$4+$5} END {print sum}')
+            yield=$(grep rate /scratch/`whoami`/datacards/datacards_${Model}/datacard_${region}_m*_${Model}.txt | awk '{ sum+=$2} END {print sum}')
+            bkg=$(grep rate /scratch/`whoami`/datacards/datacards_${Model}/datacard_${region}_m*_${Model}.txt | awk '{ sum+=$3+$4+$5} END {print sum}')
 	    echo $region $limit $obslimit $yield $bkg>> AsymptoticRanking_${Model}_Paper_post.txt
 	    
 	    echo $region >> AsymptoticRanking_${Model}_Paper_post_plusBG.txt
-	    for d in $(ls -rt /scratch/`whoami`/datacards_${Model}/datacard_${region}_m*)
+	    for d in $(ls -rt /scratch/`whoami`/datacards/datacards_${Model}/datacard_${region}_m*)
 	    do
 		bin=${d##*${region}_}
 		bin=${bin%_${Model}.txt}
-		thisyield=$(grep rate /scratch/`whoami`/datacards_${Model}/datacard_${region}_${bin}_${Model}.txt | awk '{ sum=$2} END {print sum}')
-		thisbkg=$(grep rate /scratch/`whoami`/datacards_${Model}/datacard_${region}_${bin}_${Model}.txt | awk '{ sum=$3+$4+$5} END {print sum}')
+		thisyield=$(grep rate /scratch/`whoami`/datacards/datacards_${Model}/datacard_${region}_${bin}_${Model}.txt | awk '{ sum=$2} END {print sum}')
+		thisbkg=$(grep rate /scratch/`whoami`/datacards/datacards_${Model}/datacard_${region}_${bin}_${Model}.txt | awk '{ sum=$3+$4+$5} END {print sum}')
 		echo $bin '&' $thisyield '&' $thisbkg '\\'>>AsymptoticRanking_${Model}_Paper_post_plusBG.txt
 	    done
 
@@ -32,4 +36,4 @@ do
     fi
 done
 
-#echo `rm -f log_${Model}*ANvApril*`
+cd -
